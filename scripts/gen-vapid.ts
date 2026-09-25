91270043346411e42e8b93d8e71c1a5ad80afda2
‎ ‎      ‏‏‎ <edsonraga1234@gmail.com>
2026-09-24 19:48:45 -0600

    Revert "upload 1.0"
    This reverts commit f4059b802e4927050cbbee72863229dda0d8ed63.

--- scripts/gen-vapid.ts
+++
@@ -1,5 +0,0 @@
-import webpush from "web-push";
-
-const vapidKeys = webpush.generateVAPIDKeys();
-console.log("VAPID_PUBLIC_KEY=" + vapidKeys.publicKey);
-console.log("VAPID_PRIVATE_KEY=" + vapidKeys.privateKey);
